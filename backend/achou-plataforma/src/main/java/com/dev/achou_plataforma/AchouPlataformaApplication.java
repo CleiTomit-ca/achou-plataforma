f5bf692be79929b4bf27721aac45661ca90236f6
Cleiton Ferreira Silva <mitonferreira788@gmail.com>
2026-09-04 21:13:28 -0300

    fix(pasta): recolocando arquivos

---
+++ backend/achou-plataforma/src/main/java/com/dev/achou_plataforma/AchouPlataformaApplication.java
@@ -0,0 +1,13 @@
+package com.dev.achou_plataforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchouPlataformaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchouPlataformaApplication.class, args);
+	}
+
+}
